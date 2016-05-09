ecb8363b28067fdf35b651bb8b7a738dbefeb937
yaronthurm <yaronthurm@gmail.com>
2016-05-09 21:39:49 +0300

    added grain client to migration service

--- MigrationService/Program.cs
+++ MigrationService/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Orleans;
+using Orleans.Runtime.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,15 @@ namespace MigrationService
     {
         static void Main(string[] args)
         {
+            var deploymentId = args[0];
+            int gatewayPort = int.Parse(args[1]);
+            GrainClient.Initialize(new ClientConfiguration
+            {
+                DeploymentId = deploymentId,
+                Gateways = new[] { new IPEndPoint(IPAddress.Loopback, gatewayPort) }
+            });
+
+
         }
     }
 }
