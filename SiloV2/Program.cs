6344aba5ec5765f778ed8b246ddaf6de085ff29e
yaronthurm <yaronthurm@gmail.com>
2016-05-10 20:33:16 +0300

    added silo v2

--- SiloV2/Program.cs
+++ SiloV2/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
+﻿using Orleans.Runtime.Host;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SiloV2
+namespace SiloV1
 {
     class Program
     {
+
+        static SiloHost siloHost;
+
         static void Main(string[] args)
         {
+            // Orleans should run in its own AppDomain, we set it up like this
+            AppDomain hostDomain = AppDomain.CreateDomain("OrleansHost", null,
+                new AppDomainSetup()
+                {
+                    AppDomainInitializer = InitSilo
+                });
+
+            Console.WriteLine("Orleans Silo is running.\nPress Enter to terminate...");
+            Console.ReadLine();
+
+            // We do a clean shutdown in the other AppDomain
+            hostDomain.DoCallBack(ShutdownSilo);
         }
+
+        static void InitSilo(string[] args)
+        {
+            siloHost = new SiloHost(System.Net.Dns.GetHostName());
+            // The Cluster config is quirky and weird to configure in code, so we're going to use a config file
+            siloHost.ConfigFileName = "OrleansConfiguration.xml";
+
+            siloHost.InitializeOrleansSilo();
+            var startedok = siloHost.StartOrleansSilo();
+            if (!startedok)
+                throw new SystemException(String.Format("Failed to start Orleans silo '{0}' as a {1} node", siloHost.Name, siloHost.Type));
+
+        }
+
+        static void ShutdownSilo()
+        {
+            if (siloHost != null)
+            {
+                siloHost.Dispose();
+                GC.SuppressFinalize(siloHost);
+                siloHost = null;
+            }
+        }
+
     }
 }
