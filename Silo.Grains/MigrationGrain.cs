46105f9a74f6fffa96fd50143c075ad6dc4b97ed
yaronthurm <yaronthurm@gmail.com>
2016-05-11 13:51:56 +0300

    added untracked files to git

---
+++ Silo.Grains/MigrationGrain.cs
@@ -0,0 +1,52 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace Silo.Grains
+{
+    public interface IMigrationGrain : IGrainWithStringKey
+    {
+        Task<bool> IsMigrated();
+
+        Task EnsureMigrated();
+    }
+
+    public class MigrationGrain : Grain, IMigrationGrain
+    {
+        private StorageClient _storageClient = new StorageClient("localhost", 50200);
+        private bool _isMigrated;
+
+        public override async Task OnActivateAsync()
+        {
+            var storageRes = await _storageClient.FindAsync("Migration", this.GetPrimaryKeyString());
+            _isMigrated = storageRes.Found;
+            await base.OnActivateAsync();
+        }
+
+        public Task<bool> IsMigrated()
+        {
+            return Task.FromResult(_isMigrated);
+        }
+
+        public async Task EnsureMigrated()
+        {
+            if (_isMigrated)
+                return;
+
+            await PerformMigration();
+        }
+
+        private async Task PerformMigration()
+        {
+            var httpClient = new HttpClient();
+            await httpClient.GetStringAsync($"http://localhost:50200/SiloV1/Deactivate/{this.GetPrimaryKeyString()}");
+            _isMigrated = true;
+            await _storageClient.SaveAsync("Migration", this.GetPrimaryKeyString(), "");
+        }
+    }
+}
---
+++ StorageService/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace StorageService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server();
+            server.Start();
+
+            Console.WriteLine("Started Storage service.\nPress Enter to terminate...");
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Server : HttpServer
+    {
+        public static ConcurrentDictionary<string, string> Data = new ConcurrentDictionary<string, string>();
+
+        public Server() : base("localhost", 50200, "Storage") { }
+
+        protected override async Task<HttpResponse> ProcessRequestAsync(HttpListenerContext context)
+        {
+            // Expected "/Storage/Save/<id>/<value>"  or
+            //          "/Storage/Get/<id>
+
+            var path = context.Request.RawUrl.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+            if (path.Length < 2) return new HttpResponse { StatusCode = HttpStatusCode.BadRequest, Body = "Invalid url format" };
+
+            var method = path[1];
+            switch (method)
+            {
+                case "Save":
+                    if (path.Length != 4) return new HttpResponse { StatusCode = HttpStatusCode.BadRequest, Body = "Invalid url format. Expected /Storage/Save/<id>/<value>" };
+                    Data[path[2]] = path[3];
+                    return new HttpResponse { StatusCode = HttpStatusCode.OK, Body = "" };
+
+                case "Get":
+                    if (path.Length != 3) return new HttpResponse { StatusCode = HttpStatusCode.BadRequest, Body = "Invalid url format. Expected /Storage/Get/<id>" };
+                    string val;
+                    if (Data.TryGetValue(path[2], out val))
+                        return new HttpResponse { StatusCode = HttpStatusCode.OK, Body = val.ToString() };
+                    else
+                        return new HttpResponse { StatusCode = HttpStatusCode.NotFound, Body = "" };
+
+                default: return new HttpResponse { StatusCode = HttpStatusCode.BadRequest, Body = "Invalid method" };
+            }
+        }
+    }
+}
